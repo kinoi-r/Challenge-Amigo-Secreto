1553da7664a3db63991851a60057cb2d0ff52a48
Kassiany <kassianyluzti@gmail.com>
2025-03-18 15:11:12 -0300

    Adicionando função para adicionar amigo

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 const listaAmigos = [];
+
+function adicionarAmigo() {
+    const input = document.getElementById("Amigo");
+    const nome = input.ariaValueMax.trim();
+    const lista = document.getElementById("listaAmigos");
+}
