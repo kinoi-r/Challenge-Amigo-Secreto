3b79d2574a117c46defb06f43d28ad959a7cbd9d
Kassiany <kassianyluzti@gmail.com>
2025-03-18 15:13:11 -0300

    Adicionando validação de nome vazio

--- app.js
+++ app.js
@@ -4,4 +4,10 @@ function adicionarAmigo() {
     const input = document.getElementById("Amigo");
     const nome = input.ariaValueMax.trim();
     const lista = document.getElementById("listaAmigos");
+
+    if (nome === "") {
+        alert("Por favor, insira um nome válido.");
+        return;
+    }
+
 }
