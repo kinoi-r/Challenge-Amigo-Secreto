95978b0a1299a27771a20f01b250922e469b8e7b
Kassiany <kassianyluzti@gmail.com>
2025-03-18 15:49:15 -0300

    Correção nas variáveis

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const listaAmigos = [];
 
 function adicionarAmigo() {
     const input = document.getElementById("Amigo");
-    const nome = input.ariaValueMax.trim();
+    const nome = input.value.trim();
     const lista = document.getElementById("listaAmigos");
 
     if (nome === "") {
